449f76b88f6551238f3309ec36d988a11a826c7e
Atish1456 <145015554+Atish1456@users.noreply.github.com>
2024-02-17 14:20:26 +0530

    simple web

---
+++ SimpleWebsite/index.js
@@ -0,0 +1,14 @@
+let btn = document.getElementById("btn1")
+
+btn.addEventListener("click", function () {
+    
+    alert("logged")
+})
+
+let btn1= document.getElementById("btn2")
+btn1.addEventListener("click",function(){
+    alert("signed");
+})
+
+let main= document.getElementById("main")
+main.style.cursor="pointer"
