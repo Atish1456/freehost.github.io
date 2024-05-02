6ac10d7314c915251d77b5037665d02a956edd11
Atish1456 <145015554+Atish1456@users.noreply.github.com>
2024-05-02 11:17:42 +0530

    Add files via upload
    Gsap_S-webstie

---
+++ 07_Gsap_S-website/index.js
@@ -0,0 +1,32 @@
+var time = gsap.timeline()
+time.to("#nav-center", {
+    y: 200,
+    duration: 1,
+    delay: 1,
+    
+})
+time.from("#h",{
+    
+    x:-1120,
+    duration:1,
+    delay:0,
+
+})
+time.to("#sec img",{
+    x:190,
+    duration:1,
+    delay:1,
+
+})
+time.to("#sec2 img",{
+    x:190,
+    duration:1,
+  
+
+})
+time.to("#sec3 img",{
+    x:190,
+    duration:1,
+
+
+})
