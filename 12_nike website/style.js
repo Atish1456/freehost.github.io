a9130ae0c266f624207456f3a659c9cde82525b1
Atish1456 <145015554+Atish1456@users.noreply.github.com>
2024-05-02 12:00:45 +0530

    NIKE-CLONE

---
+++ 12_nike website/style.js
@@ -0,0 +1,11 @@
+let show = document.querySelector("#riya");
+let hide = document.querySelector("#navmedia");
+let hide1 = document.querySelector("#suku");
+
+show.addEventListener('click', function () {
+    hide.style.display = "block"
+})
+
+hide1.addEventListener('click', function () {
+    hide.style.display = "none"
+})
